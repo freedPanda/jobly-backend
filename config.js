78f47c2bb4b2690f7490a9781336a65073bcf985
Chris H <pandawonprogrammer@gmail.com>
2020-10-25 18:05:24 -0500

    d

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 const SECRET = process.env.SECRET_KEY || 'test';
 
 const PORT = process.env.PORT || 3001;
-
+console.log(PORT);
 // database is:
 //
 // - on Heroku, get from env var DATABASE_URL
